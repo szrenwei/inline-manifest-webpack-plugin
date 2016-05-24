35053ab496704b76b90667b0d4b3f7711b42a995
szrenwei <wei.ren@dianrong.com>
2016-05-24 15:56:40 +0800

    add eslint

--- index.js
+++ index.js
@@ -1,28 +1,27 @@
 var sourceMappingURL = require('source-map-url')
 
-function InlineManifestPlugin(options) {
-	this.options = extend({
-		name: 'webpackManifest',
-		separator: '-'
-	}, options)
+function InlineManifestPlugin (options) {
+    this.options = extend({
+        name: 'webpackManifest',
+        separator: '-'
+    }, options)
 }
 
-InlineManifestPlugin.prototype.apply = function(compiler) {
-	var me = this
+InlineManifestPlugin.prototype.apply = function (compiler) {
+    var me = this
 
-    compiler.plugin('compilation', function(compilation){
-        compilation.plugin('html-webpack-plugin-before-html-generation', function(htmlPluginData, callback) {
-            var webpackManifest,
-				name = me.options.name
-			// HtmlWebpackPlugin use the 'manifest' name as HTML5's app cache manifest
-			// so we can't use the same name
-			if(name === 'manifest')
-				throw new Error('[InlineManifestWebpackPlugin]: name can\'t be "manifest", please change another')
+    compiler.plugin('compilation', function (compilation) {
+        compilation.plugin('html-webpack-plugin-before-html-generation', function (htmlPluginData, callback) {
+            var webpackManifest
+            var name = me.options.name
+            // HtmlWebpackPlugin use the 'manifest' name as HTML5's app cache manifest
+            // so we can't use the same name
+            if (name === 'manifest') throw new Error('[InlineManifestWebpackPlugin]: name can\'t be "manifest", please change another')
 
-            for(var key in compilation.assets){
-                if(key.indexOf('manifest' + me.options.separator) > -1){
-					// remove sourceMap url if exist
-					webpackManifest = sourceMappingURL.removeFrom(compilation.assets[key].source())
+            for (var key in compilation.assets) {
+                if (key.indexOf('manifest' + me.options.separator) > -1) {
+                    // remove sourceMap url if exist
+                    webpackManifest = sourceMappingURL.removeFrom(compilation.assets[key].source())
                     break
                 }
             }
@@ -33,20 +32,20 @@ InlineManifestPlugin.prototype.apply = function(compiler) {
     })
 }
 
-function extend(base){
-	var i = 1,
-		len = arguments.length
+function extend (base) {
+    var i = 1
+    var len = arguments.length
 
-	for( ;i < len; i++){
-		var obj = arguments[i]
-		for(var key in obj){
-			if(obj.hasOwnProperty(key)){
-				base[key] = obj[key]
-			}
-		}
-	}
+    for (; i < len; i++) {
+        var obj = arguments[i]
+        for (var key in obj) {
+            if (obj.hasOwnProperty(key)) {
+                base[key] = obj[key]
+            }
+        }
+    }
 
-	return base
+    return base
 }
 
 module.exports = InlineManifestPlugin
